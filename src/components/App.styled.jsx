820a581a9388c4f3bae6946ee1192e224a8cfa03
OlenaMurza <murzaelena@gmail.com>
2023-02-25 23:54:51 +0200

    add

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+
+export const Container = styled.div`
+  padding: 15px 30px;
+`;
+
+export const Section = styled.section`
+  &:not(:last-child) {
+    margin-bottom: 15px;
+  }
+`;
+
+export const TitleH1 = styled.h1`
+  margin-bottom: 15px;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+export const TitleH2 = styled.h2`
+  margin-bottom: 15px;
+  font-size: 30px;
+  font-weight: bold;
+`;
